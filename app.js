c95e1912c16917fdf8c2e31b4da42d43d4835d97
monameister <monkhnaran@gmail.com>
2022-05-04 01:12:20 +0800

    Shoog shiddeg bolgov

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ var playerScore = [0, 0];
 var currentScore = 0;
 
 // Шооны аль талаараа буусныг шалгах хувьсагч, тэр нь 1-6 гэсэн утгыг санамсаргүйгээр авна.
-var dice = Math.floor(Math.random() * 6 + 1);
 
 // Программ эхэлхэд бэлтгэе!
 document.getElementById("score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-1").textContent = 0;
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-0").textContent = 0;
-document.querySelector(".dice").style.display = "none";
 
-console.log(dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6 + 1);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
