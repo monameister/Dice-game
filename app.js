67ebdbbef328f618020b155259254e1c3c63a9e3
monameister <monkhnaran@gmail.com>
2022-05-05 23:49:38 +0800

    togloom buren duussan

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var activePlayer;
-var playerScore = [];
+var playerScore;
 var currentScore;
+var isNewGame;
 
 //Шоог олон хайхаас сэргийлж хувьсагчид хадгална
 var diceDom = document.querySelector(".dice");
@@ -10,6 +11,8 @@ newGame();
 
 //Тоглоомыг шинээр эхлүүлэх функц
 function newGame() {
+  isNewGame = true;
+
   // Тоглогчдын ээлжийг хадгалдаг хувьсагч
   activePlayer = 0;
 
@@ -37,47 +40,52 @@ function newGame() {
 
 //Шоо шидэх event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  //1-6 хүртэл тоог рандомоор гаргаж авна.
-  var diceNumber = Math.floor(Math.random() * 6 + 1);
+  if (isNewGame === true) {
+    //1-6 хүртэл тоог рандомоор гаргаж авна.
+    var diceNumber = Math.floor(Math.random() * 6 + 1);
 
-  // Шоог ил гаргана.
-  diceDom.style.display = "block";
-  // Рандомоор гаргаж авсан тоонд харгалзах шооны зургийг харуулна.
-  diceDom.src = "dice-" + diceNumber + ".png";
+    // Шоог ил гаргана.
+    diceDom.style.display = "block";
+    // Рандомоор гаргаж авсан тоонд харгалзах шооны зургийг харуулна.
+    diceDom.src = "dice-" + diceNumber + ".png";
 
-  // Тоглогчийн ээлжийн оноог өөрчлөх нөхцөл
-  if (diceNumber !== 1) {
-    // Шооны буусан тоог ээлжийн онооны хувьсагчид нэмнэ.
-    currentScore = currentScore + diceNumber;
+    // Тоглогчийн ээлжийн оноог өөрчлөх нөхцөл
+    if (diceNumber !== 1) {
+      // Шооны буусан тоог ээлжийн онооны хувьсагчид нэмнэ.
+      currentScore = currentScore + diceNumber;
 
-    // Ээлжийн онооны хувьсагч дахь тоог идэвхитэй тоглогчид харуулна
-    document.getElementById("current-" + activePlayer).textContent =
-      currentScore;
-  } else {
-    // 1 буусан тохиолдолд ээлжийг сольно.
-    swithPlayers();
+      // Ээлжийн онооны хувьсагч дахь тоог идэвхитэй тоглогчид харуулна
+      document.getElementById("current-" + activePlayer).textContent =
+        currentScore;
+    } else {
+      // 1 буусан тохиолдолд ээлжийг сольно.
+      swithPlayers();
+    }
   }
 });
 
 //hold товчийн домоос олж эвэнт листенер холбоно.
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // Хэрэв тоглогчийн оноо 0 биш үед товчлуур ажиллана.
-  if (currentScore !== 0) {
-    // Тухайн тоглочийн ээлжийн оноог идэвхитэй тоглогчийн оноон дээр нэмнэ.
-    playerScore[activePlayer] = playerScore[activePlayer] + currentScore;
+  if (isNewGame === true) {
+    // Хэрэв тоглогчийн оноо 0 биш үед товчлуур ажиллана.
+    if (currentScore !== 0) {
+      // Тухайн тоглочийн ээлжийн оноог идэвхитэй тоглогчийн оноон дээр нэмнэ.
+      playerScore[activePlayer] = playerScore[activePlayer] + currentScore;
 
-    // Нэмсэн оноог тоглогчийн самбарт харуулна
-    document.getElementById("score-" + activePlayer).textContent =
-      playerScore[activePlayer];
+      // Нэмсэн оноог тоглогчийн самбарт харуулна
+      document.getElementById("score-" + activePlayer).textContent =
+        playerScore[activePlayer];
 
-    // Хэрэв тухайн тоглогчийн оноо 100-с их болсон үед ялагч болно.
-    if (playerScore[activePlayer] >= 10) {
-      document.getElementById("name-" + activePlayer).textContent = "WINNER!";
-      document
-        .querySelector(".player-" + activePlayer + "-panel")
-        .classList.add("winner");
-    } else {
-      swithPlayers();
+      // Хэрэв тухайн тоглогчийн оноо 100-с их болсон үед ялагч болно.
+      if (playerScore[activePlayer] >= 10) {
+        document.getElementById("name-" + activePlayer).textContent = "WINNER!";
+        document
+          .querySelector(".player-" + activePlayer + "-panel")
+          .classList.add("winner");
+        isNewGame = false;
+      } else {
+        swithPlayers();
+      }
     }
   }
 });
